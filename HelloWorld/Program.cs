8f4d243137ca632270860c60f501834567d47b40
gustavo0867 <gbrasilreis08@gmail.com>
2025-02-17 11:55:59 -0400

    update

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,59 @@
+﻿using System; 
+
+class URI {
+
+        public static void Main(string[] args) { 
+
+            
+        int a = LerNumero();
+        int b= LerNumero();
+
+        Console.WriteLine("A soma é: "+soma(a,b));
+        
+    
+
+    }
+
+    static int LerNumero (){
+
+        int numeroValidado;
+        string? entrada;
+
+        while(true){
+            Console.WriteLine("Digite um número: ");
+            entrada = Console.ReadLine();
+
+            if (int.TryParse(entrada, out numeroValidado))
+                {
+                return numeroValidado; // Sai do loop e retorna o valor se for válido
+                }
+            else
+                {
+                    Console.WriteLine("Número inválido. Digite novamente."); 
+                }
+
+        }
+
+    }
+
+
+
+    static int soma(int a, int b){
+        int soma= a+b;
+
+        return soma;
+    }
+
+}
+
+
+
+
+
+
+
+int[]  teste = new int[5]
+
+
+
+string[] testando= {"1"}
---
+++ HelloWorld/Teste/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] votes = new int[] { 1, 3, 5, 4, 1, 3, 1, 2};
+var count = 0;
+foreach (var vote in votes)
+{
+    if (vote > 3){
+    Console.WriteLine("ok");
+        continue;
+        }
+
+
+
+    count++;
+}
+Console.WriteLine(count + " votos válidos");

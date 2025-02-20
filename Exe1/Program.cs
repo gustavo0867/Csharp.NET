c4ea0da2f13844ef4ab72751da754b3238a3d2ab
gustavo0867 <gbrasilreis08@gmail.com>
2025-02-20 07:15:40 -0400

    update

---
+++ Exe1/Program.cs
@@ -0,0 +1,30 @@
+﻿public static void CheckAge(int Age)
+{
+   if (Age < 18) throw new AccessViolationException("Pessoa menor de idade");
+
+}
+
+
+public static void Main(string[] args)
+{
+    try
+    {
+        InitialGreeting();
+        int year = Convert.ToInt32(Console.ReadLine());
+        var age = CalculateAgeByYear(year);
+        Console.WriteLine("Idade: " + age);
+        CheckAge(age);
+    }
+    catch(AccessViolationException err)
+    {
+        Console.WriteLine("Erro de permissão: " + err.Message);
+    }
+    catch(Exception err)
+    {
+        Console.WriteLine("Erro: " + err.Message);
+    }
+    finally 
+    {
+        Console.WriteLine("Fim do software");
+    }
+}
